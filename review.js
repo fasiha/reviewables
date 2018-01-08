ad3d0d57a3090c196761defd2d1fba0a56f4a893
Ahmed Fasih <wuzzyview@gmail.com>
2018-01-08 09:23:26 -0500

    Grading in

--- review.js
+++ review.js
@@ -6,6 +6,7 @@ var bs = require("binary-search");
 var shuffle = require('lodash.shuffle');
 
 var ruby = require('./ruby');
+var cliPrompt = require('./cliPrompt');
 
 //
 // PARSING
@@ -39,14 +40,14 @@ async function parseFile(filename) {
 //
 function pickReviewable(reviewables) {
   let reviewable = reviewables[Math.floor(Math.random() * reviewables.length)];
-  let subreview = Math.random() < 0.995 ? "kanji" : "reading";
+  let subreview = Math.random() < 0.5 ? "kanji" : "reading";
   return { reviewable, subreview };
 }
 
 //
 // Display review
 //
-function displayReviewable(review, reviewables) {
+function presentQuiz(review, reviewables) {
   if (review.subreview === 'kanji') {
     console.log(`Which of the following is the kanji for: ${ruby.furiganaStringToReading(review.reviewable.fact)}`);
     let randIdx = Array.from(Array(4), (_, i) => Math.floor(Math.random() * reviewables.length));
@@ -58,11 +59,55 @@ function displayReviewable(review, reviewables) {
   }
 }
 
+//
+// Grade and display
+//
+function headerToHash(header) {
+  let res = header.match(/#+\s*(.*)/);
+  return res ? '#' + res[1] : '';
+}
+function validateNumber(input) { return (input.search(/^[0-9]+$/) === 0) ? parseInt(input) : NaN; }
+async function gradeAndDisplay(result, quiz, review, reviewables) {
+  if (result.indexOf('?') >= 0) {
+    // don't know
+    console.log(`${ruby.furiganaStringToPlain(review.reviewable.fact)} : ${
+        ruby.furiganaStringToReading(review.reviewable.fact)}`);
+    console.log(`Visit ${headerToHash(review.reviewable.header)}`);
+    return;
+  }
+  if (review.subreview === 'kanji') {
+    let idx = validateNumber(result) - 1;
+    let selected = quiz[idx];
+    if (selected && ruby.furiganaStringToPlain(selected.fact) === ruby.furiganaStringToPlain(review.reviewable.fact)) {
+      console.log('¡¡¡You juiced it!!! 😍');
+    } else {
+      if (selected) {
+        // You actually selected the wrong answer
+        console.log('You pooped it 😭…');
+        console.log(`I was looking for: ${ruby.furiganaStringToPlain(review.reviewable.fact)}`);
+      } else {
+        console.log('Um, u ok?');
+      }
+    }
+  } else {
+    if (result === ruby.furiganaStringToReading(review.reviewable.fact)) {
+      console.log('¡¡¡You juiced it!!! 😍');
+    } else {
+      console.log('You pooped it 😭…');
+      console.log(`I was looking for: ${ruby.furiganaStringToReading(review.reviewable.fact)}`);
+    }
+  }
+}
+
 // Parse file -> pick which to review -> display review (with confusers, etc.) -> grade and display result -> log review
 if (require.main === module) {
   (async function() {
     let reviewables = await parseFile('Toponyms.md');
     let pickedForReview = pickReviewable(reviewables);
-    let quizDetails = displayReviewable(pickedForReview, reviewables);
+    let quizDetails = presentQuiz(pickedForReview, reviewables);
+    // console.log(pickedForReview);
+    // console.log(reviewables[0]);
+    let enteredText = await cliPrompt();
+    gradeAndDisplay(enteredText, quizDetails, pickedForReview, reviewables);
   })();
 }
